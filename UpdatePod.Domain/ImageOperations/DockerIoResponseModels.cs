8d7f8c7266fd9f6174fa8dc6562ca10dc6d11bf2
Anil Kaynar <anilkaynar@Anils-MacBook-Pro.local>
2025-07-14 00:54:35 +0300

    New Test Project added to sln and also DockerIoImageResponseModels classes is convert to string

--- UpdatePod.Domain/ImageOperations/DockerIoResponseModels.cs
+++ UpdatePod.Domain/ImageOperations/DockerIoResponseModels.cs
@@ -1,36 +1,33 @@
 namespace UpdatePod.Domain.ImageOperations;
 
-public class DockerIoImageResponseModels
-{
-    public int? creator { get; set; }
-    public int? id { get; set; }
-    public Images[]? images { get; set; }
-    public string? last_updated { get; set; }
-    public int? last_updater { get; set; }
-    public string? last_updater_username { get; set; }
-    public string? name { get; set; }
-    public int? repository { get; set; }
-    public int? full_size { get; set; }
-    public bool? v2 { get; set; }
-    public string? tag_status { get; set; }
-    public string? tag_last_pulled { get; set; }
-    public string? tag_last_pushed { get; set; }
-    public string? media_type { get; set; }
-    public string? content_type { get; set; }
-    public string? digest { get; set; }
-}
+public record DockerIoImageResponseModels(
+    int? creator,
+    int? id,
+    Images[]? images,
+    string? last_updated,
+    int? last_updater,
+    string? last_updater_username,
+    string? name,
+    int? repository,
+    int? full_size,
+    bool? v2,
+    string? tag_status,
+    string? tag_last_pulled,
+    string? tag_last_pushed,
+    string? media_type,
+    string? content_type,
+    string? digest
+);
 
-public class Images
-{
-    public string? architecture { get; set; }
-    public string? features { get; set; }
-    public string? variant { get; set; }
-    public string? digest { get; set; }
-    public string? os { get; set; }
-    public string? os_features { get; set; }
-    public int? size { get; set; }
-    public string? status { get; set; }
-    public string? last_pulled { get; set; }
-    public string? last_pushed { get; set; }
-}
-
+public record Images(
+    string? architecture,
+    string? features,
+    string? variant,
+    string? digest,
+    string? os,
+    string? os_features,
+    int? size,
+    string? status,
+    string? last_pulled,
+    string? last_pushed
+);
