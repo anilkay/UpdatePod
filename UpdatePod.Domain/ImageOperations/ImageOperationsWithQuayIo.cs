d0a2a9bdb62b27c6a0ae6f28daad402e6f78da27
Anil Kaynar <anilkaynar@Anils-MacBook-Pro.local>
2025-07-22 23:24:27 +0300

    ImageOperationsWithQuayIo add summaries and when return v1 manifest throw exception.

--- UpdatePod.Domain/ImageOperations/ImageOperationsWithQuayIo.cs
+++ UpdatePod.Domain/ImageOperations/ImageOperationsWithQuayIo.cs
@@ -4,6 +4,14 @@ namespace UpdatePod.Domain.ImageOperations;
 
 public class ImageOperationsWithQuayIo(HttpClient client): IImageOperationsStrategy
 {
+    /// <summary>
+    /// Get Latest Hash From Quay.io. Only Supports Manifest Version 2 Images.
+    /// </summary>
+    /// <param name="repository"></param>
+    /// <param name="tag"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    /// <exception cref="NotSupportedException"></exception>
     public async  Task<string?> GetLatestHash(string repository, string tag, CancellationToken token)
     {
         var url= GetUrl(repository, tag);
@@ -19,11 +27,27 @@ public class ImageOperationsWithQuayIo(HttpClient client): IImageOperationsStrat
 
         var responseAsStr=await response.Content.ReadAsStringAsync();
         
+        var canGetContentTypeHeader = response.Content.Headers.TryGetValues("Content-Type", out var contentType);
+
+        if (canGetContentTypeHeader)
+        {
+            if (contentType?.FirstOrDefault() == "application/vnd.docker.distribution.manifest.v1+json")
+            {
+                throw new NotSupportedException("V1 Manifest Images is not supported");
+            }
+        }
+        
         var canGetHeader=response.Headers.TryGetValues("docker-content-digest", out var digest);
 
         return !canGetHeader ? null : digest?.ToList().FirstOrDefault();
     }
-    
+    /// <summary>
+    /// Create Url from given imagename and tag.
+    /// </summary>
+    /// <param name="imageName"></param>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
     private string GetUrl(string imageName, string tag)
     {
         var parts = imageName.Split('/');
